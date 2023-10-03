bfb58350a0b436869dab3135ac762ed885412ba4
Felipe Juaneda <felipejua9@gmail.com>
2023-10-03 16:04:46 -0300

    cambios en portfolio, app.js. Ahora cambio en form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Contact from "./components/Contact/Contact";
 import Education from "./components/EduExp/Education/Education";
@@ -7,35 +6,34 @@ import Experience from "./components/EduExp/Experience/Experience";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Proyects from "./components/Proyects/Proyects";
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Home />
-                <Proyects />
-                <Contact />
-              </>
-            }
-          >
-            <Route path="experience" element={<Experience />} />
-            <Route path="education" element={<Education />} />
-          </Route>
-          <Route path="/aboutme" element={<AboutMe />}>
-            <Route path="experience" element={<Experience />} />
-            <Route path="education" element={<Education />} />
-          </Route>
-          <Route path="/proyects" element={<Proyects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <Proyects />
+              <Contact />
+            </>
+          }
+        >
+          <Route path="experience" element={<Experience />} />
+          <Route path="education" element={<Education />} />
+        </Route>
+        <Route path="/aboutme" element={<AboutMe />}>
+          <Route path="experience" element={<Experience />} />
+          <Route path="education" element={<Education />} />
+        </Route>
+        <Route path="/proyects" element={<Proyects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
