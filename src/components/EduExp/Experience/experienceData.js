2b2f1f395a1727c5f2ed91de9f4233aeefa53b89
Felipe Juaneda <felipejua9@gmail.com>
2024-04-12 16:47:26 -0300

    ajustes visuales en experrience

--- src/components/EduExp/Experience/experienceData.js
+++ src/components/EduExp/Experience/experienceData.js
@@ -5,7 +5,7 @@ const infoExp = [
     logo: "https://comparte-entity-photos.s3.us-east-2.amazonaws.com/39144702-7af9-45a6-8e18-3eb28648fa8a.png",
     title: "Tutor React Js",
     classNameFather:
-      "w-full h-full px-4 py-4 mt-6 bg-gray-800 rounded-lg shadow-lg lg:w-1/2 ",
+      "w-full h-full mt-2 sm:mt-5 lg:mt-24 px-4 py-4  bg-gray-800 rounded-lg shadow-lg lg:w-1/2 ",
     description: `✔️Seguimiento de los alumnos durante el curso, brindando apoyo y explicación sobre los temas aplicado en clase. ✔️Respuesta a consultas fuera de clases. ✔️Corrección de desafíos prácticos de evaluación. ✔️ Guía en la realización de proyectos finales. ✔️Orientación constante y permanente sobre el uso de herramientas. ✔️Más de 300 entregas corregidas`,
 
     duration: "ago. 2022 - act.",
@@ -18,7 +18,7 @@ const infoExp = [
     logo: "https://i.ibb.co/bsd1Gck/logo-Outside-The-Box-Photoroom-png-Photoroom.png",
     title: "Programador full stack",
     classNameFather:
-      "w-full h-full px-4 py-4 mt-6 bg-gray-800 rounded-lg shadow-lg lg:w-1/2 ",
+      "w-full h-full mt-6 px-4 py-4 bg-gray-800 rounded-lg shadow-lg lg:w-1/2 ",
     description: `Trabajo en estrecha colaboración con el equipo de diseño y desarrollo para implementar funcionalidades clave y mejorar la usabilidad de la aplicación, de una empresa que ofrece envíos de paquetería. 
     Mi objetivo es asegurar que los usuarios puedan navegar fácilmente por la plataforma, realizar seguimiento de envíos y gestionar sus operaciones logísticas de manera eficiente.`,
 
@@ -32,7 +32,7 @@ const infoExp = [
     logo: "https://i.ibb.co/DgX4MGC/Group-41.png",
     title: "Desarrollador de front-end",
     classNameFather:
-      "w-full h-full px-4 py-4 mt-6 bg-gray-800 rounded-lg shadow-lg lg:w-1/2 ",
+      "w-full h-full mt-2 sm:mt-5 px-4 py-4  bg-gray-800 rounded-lg shadow-lg lg:w-1/2 ",
     description: `Como parte del equipo de desarrollo de Lemur School, lideré la creación de una plataforma innovadora que potencia la marca personal de los usuarios, a través de cursos educativos. Utilizando Next.js y Firebase como pilares tecnológicos. Además, implementé Material-UI para ofrecer una experiencia de usuario intuitiva y visualmente atractiva.`,
 
     duration: "ene. 2024 - abr. 2024",
@@ -45,7 +45,7 @@ const infoExp = [
     logo: "https://i.ibb.co/LgGBjxP/dwilliamsre-logo-Photoroom-png-Photoroom.png",
     title: "Desarrollador de front-end",
     classNameFather:
-      "w-full h-full px-4 py-4 mt-6 bg-gray-800 rounded-lg shadow-lg lg:w-1/2 ",
+      "w-full h-full mt-2 sm:mt-5 xl:mt-24 px-4 py-4  bg-gray-800 rounded-lg shadow-lg lg:w-1/2 ",
     description: `Como encargado del desarrollo del panel de administración en D' Williams Inmobiliaria, lideré la creación del sistema centralizado utilizado por agentes inmobiliarios. La plataforma, integrada con Firebase, ha sido fundamental para facilitar una colaboración eficiente entre los miembros del equipo y permitir la actualización en tiempo real de la extensa cartera de propiedades.`,
 
     duration: "oct. 2023 - dic. 2023",
